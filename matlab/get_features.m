db5d5de999b43d125dc0815ed3490cebc31f810a
Gregory Ditzler <gregory.ditzler@gmail.com>
2014-02-17 16:47:51 -0500

    update feature scripts

--- matlab/get_features.m
+++ matlab/get_features.m
@@ -1,4 +1,4 @@
-function V = get_features(data, labels, numToSelect, method)
-selectedFeatures = feast(method, numToSelect,data,labels);
+function V = get_features(data, labels, n_select, method)
+selected_features = feast(method, n_select, data, labels);
 V = zeros(1, size(data,2))';
-V(selectedFeatures) = 1;
+V(selected_features) = 1;
